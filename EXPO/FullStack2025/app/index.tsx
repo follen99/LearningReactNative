364c0e0d0afa2d6ad86e2d0fb6d499fb31dc9479
Giuliano Ranauro <74026278+follen99@users.noreply.github.com>
2025-03-30 19:19:26 +0200

    tailwind actually working

--- EXPO/FullStack2025/app/index.tsx
+++ EXPO/FullStack2025/app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Provetta</Text>
+    <View>
+      <Text className="text-3xl text-center font-bold text-blue-500 mt-10">
+        provettona
+      </Text>
     </View>
   );
 }
