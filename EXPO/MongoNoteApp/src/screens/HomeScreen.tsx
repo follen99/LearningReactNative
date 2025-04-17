1996c1f4ca609ee3cb07cc31df4e3bb1faad99e0
Giuliano Ranauro <74026278+follen99@users.noreply.github.com>
2025-04-17 13:32:23 +0200

    fix update notes

--- EXPO/MongoNoteApp/src/screens/HomeScreen.tsx
+++ EXPO/MongoNoteApp/src/screens/HomeScreen.tsx
@@ -9,18 +9,21 @@ const BASE_SERVICE_URI = "http://10.0.2.2:5000/api/notes";
 
 export default function HomeScreen({navigation}: {navigation: any}) {
   const [notes, setNotes] = useState([]);
+  const isFocused = useIsFocused();
 
   // Fetch notes from the API when the component mounts
   useEffect(() => {
-    /*
+    if (isFocused){
+      /*
     L'indirizzo 10.0.2.2 è usato per accedere al localhost dell'emulatore Android.
      */
-    console.log("STAMPA:" + BASE_SERVICE_URI + '/')
-    fetch(BASE_SERVICE_URI + '/')
-    .then(response => response.json())
-    .then(data => setNotes(data))
-    .catch(error => console.error('Error fetching notes:', error));
-  }, []);
+      console.log("STAMPA:" + BASE_SERVICE_URI + '/')
+      fetch(BASE_SERVICE_URI + '/')
+      .then(response => response.json())
+      .then(data => setNotes(data))
+      .catch(error => console.error('Error fetching notes:', error));
+    }
+  }, [isFocused]);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
